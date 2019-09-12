86ea4e413a01ede5fc4f93ca8f9c8ea8d4b02d0b
Taraspetrenko04 <Taraspetrenko04@gmail.com>
2019-09-12 15:36:49 +0300

    modal window is done

--- js/script.js
+++ js/script.js
@@ -1,63 +1,53 @@
 'use strict';
 
 window.onload = () => { //START
-    
-function addNoItems (){ // Add no Items
-    let store = document.getElementsByClassName('section__store')[0];
-    let p = document.createElement('p');
-    var text = document.createTextNode('No Items have been added');
-    p.appendChild(text);
-    store.appendChild(p);
-    store.classList.add('section__store--noItems');
-}
-addNoItems();
 
-// Button Create Form
-// function showForm() {
-//     document.getElementsByClassName('modal')[0].classList.add('active');
-// }
-// function hideForm() {
-//     document.getElementsByClassName('modal')[0].classList.remove('active');
-// }
-function toggleForm() {
-    document.getElementsByClassName('modal')[0].classList.toggle('active');
-}
-// document.getElementsByClassName('header__button')[0].addEventListener('click', function() {
-//     document.getElementsByClassName('modal')[0].classList.add('active');
-
-// })
-// Button Hide Form
-document.getElementsByClassName('button')[0].addEventListener('click', function() {
-        toggleForm();
-});
-   
-document.getElementsByClassName('modal')[0].addEventListener('click', function(event) {
-    let target = event.target;
-    console.log(target.className);
-
-    
-    if (target.className == 'modal__close' || target.className == 'modal active'){
-        toggleForm();
+    function addNoItems() { // Add no Items
+        let store = document.getElementsByClassName('section__store')[0];
+        let p = document.createElement('p');
+        var text = document.createTextNode('No Items have been added');
+        p.appendChild(text);
+        store.appendChild(p);
+        store.classList.add('section__store--noItems');
     }
-});
+    addNoItems();
+
+
+    function toggleForm() {
+        document.getElementsByClassName('modal')[0].classList.toggle('active');
+    }
+
+    // Button Hide Form
+    document.getElementsByClassName('button')[0].addEventListener('click', function () {
+        toggleForm();
+    });
+
+    document.getElementsByClassName('modal')[0].addEventListener('click', function (event) {
+        let target = event.target;
+
+        if (target.className == 'modal__close' || target.className == 'modal active') {
+            toggleForm();
+        }
+    });
+
+    // Modal doctor's select
+    document.getElementsByClassName('modal__select')[0].addEventListener('change', function () {
+        let colection = document.getElementsByTagName('input');
+        let value = document.getElementsByClassName('modal__select')[0].value;
+
+
+        for (let elem of colection) {
+            elem.classList.add('hide');
+
+
+            if (elem.name.includes(value)) {
+                elem.classList.remove('hide');
+            }
+        }
+    })
 
 
 
-// document.getElementsByTagName('body')[0].addEventListener('click', function(event) {
-//     let target = event.target;
-//     console.log(target.className);
 
-
-//     if (target.className == 'button'){
-//         toggleForm();
-//     }else if (target.className == 'modal active' || target.className == 'modal__close' ){
-//         toggleForm();
-//     } else if(target.className == 'modal__submit button '){
-//         toggleForm();
-//         // createCard()
-//     } else{}
-    
-// })
-
-// END onload
+    // END onload
 }
